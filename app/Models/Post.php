e05fcdcce3d810a365b414d071a9ce2b94f4b335
yama0408 <yama80yuuyou@gmail.com>
2023-10-16 17:21:48 +0900

    fifth commit 8-5_1016

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     
     public function getPaginateByLimit(int $limit_count = 5)
     {
@@ -20,7 +22,10 @@ class Post extends Model
     'title',
     'body',
     ];
+    
 }
+
+
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}/edit',[PostController::class, 'edit']);
 Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
 Route::get('/posts/{post}', [PostController::class,'show']);
 Route::post('/posts', [PostController::class, 'store']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
