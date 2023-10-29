3d807fb6512aeb83a30565d621e2330a66003275
yama0408 <yama80yuuyou@gmail.com>
2023-10-29 13:36:51 +0900

    last commit curriculum9 10/29

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,8 +7,11 @@ use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function boot(){
-       Paginator::useBootstrap();
+    
+    public function boot()
+    {
+        \URL::forceScheme('https');
+        $this->app['request']->server->set('HTTPS','on');
     }
 
 
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,21 +7,28 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
-        <h1 class="title">
-            {{ $post->title }}
-        </h1>
-        <div class="content">
-            <div class="content__post">
-                <h3>本文</h3>
-                <p>{{ $post->body }}</p>    
+    <x-app-layout>
+        <x-slot name="header4">
+        </x-slot>
+        <body>
+            <h1 class="title">
+                {{ $post->title }}
+            </h1>
+            
+            <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+            
+            <div class="content">
+                <div class="content__post">
+                    <h3>本文</h3>
+                    <p>{{ $post->body }}</p>    
+                </div>
             </div>
-        </div>
-        
-        <div class="edit"><a href="/posts/{{ $post->id }}/edit">edit</a></div>
-        
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
-    </body>
+            
+            <div class="edit"><a href="/posts/{{ $post->id }}/edit">edit</a></div>
+            
+            <div class="footer">
+                <a href="/">戻る</a>
+            </div>
+        </body>
+    </x-app-layout>
 </html>
